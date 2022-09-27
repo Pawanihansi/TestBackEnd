6d8eb54f2bccec3ca5b95d9a11dd038830d4f861
Pawanihansi <pawani.wit@icloud.com>
2022-09-28 01:31:25 +0530

    connect mongodb success

--- DoctorDb/server.js
+++ DoctorDb/server.js
@@ -4,10 +4,27 @@ const bodyParser = require("body-parser");
  const cors = require("cors");
  const dotenv = require("dotenv");
  const app = express();
+ require("dotenv").config();
 
- const PROT= process.env.PORT || 8070;
+ const PORT= process.env.PORT || 8070;
 
  app.use(cors());
  app.use(bodyParser.json());
 
  const URL= process.env.MONGODB_URL;
+
+ mongoose.connect(URL,{
+    /*useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopologyL:true,
+    useFindAndModify:false*/
+ });
+
+ const connection = mongoose.connection;
+ connection.once('open',()=>{
+    console.log("Mongodb Connection success!");
+ })
+
+ app.listen(PORT,()=>{
+    console.log(`Server is up and running on port number: ${PORT}`)
+ })
